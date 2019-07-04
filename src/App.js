24b9f09a94c270f60fb42d6cbfc1c4ef66c2988a
Jean-Marc Robin <jmrobin2pi@gmail.com>
2019-07-04 17:25:51 +0200

    Change greeting message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          From Grider with love.
+          From Travis with love.
         </p>
         <a
           className="App-link"
